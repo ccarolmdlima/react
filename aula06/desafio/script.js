41051e50692f6adac2c0aae1360f2671049d8aa6
ccarolmdlima <carolmdlima@sempreceub.com>
2024-09-11 22:54:17 -0300

    aaaaaaaaaa

--- aula06/desafio/script.js
+++ aula06/desafio/script.js
@@ -1,1 +1,8 @@
 const button = document.getElementById("mudar-a-cor");
+
+button.addEventListener('click', function() {
+    const curColor = document.body.style.backgroundColor;
+
+    document.body.style.backgroundColor = curColor === 'purple' ? 'white' : 'purple'; //forma compacta de if else
+    //se a cor atual for roxa, muda para branco e se a cor atual estiver branca muda para roxa
+});
