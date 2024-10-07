bdb5bb2cdf63403432e09ea3ff4d91723eb628f3
ccarolmdlima <carolmdlima@sempreceub.com>
2024-10-07 18:57:32 -0300

    aaaaaaaaa

---
+++ teste/src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header"
+
+function App() {
+  //JAVASCRIPT
+  return ( 
+  <>
+    <Header/>
+    <h1>hello world</h1>
+  </>
+  )
+}
+
+export default App
---
+++ teste/src/components/Header/index.jsx
@@ -0,0 +1,12 @@
+import './style.css'
+
+export default function Header() {
+    return (
+        <header>
+        <h1>Logo</h1>
+        <div id='header-links'>
+            <a href=''>Contato</a>
+        </div>
+        </header>
+    )
+}
